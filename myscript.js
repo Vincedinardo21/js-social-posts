9599c95be4338fb0b7a506b2592a5c6f3722313d
vince <vincedinardo21@gmail.com>
2022-05-20 16:33:38 +0200

    Creazione dei contenitori

---
+++ myscript.js
@@ -0,0 +1,20 @@
+let myContainer = document.getElementById("container");
+
+posts.forEach((element) => {
+    console.log(element);
+    let myPost = document.createElement("div");
+    let myPostHeader = document.createElement("div");
+    let myPostMeta = document.createElement("div");
+    let myPostMetaIcon = document.createElement("div");
+
+    myPost.classList.add("post");
+    myContainer.append(myPost);
+    myPostHeader.classList.add("post__header");
+    myPost.append(myPostHeader);
+    myPostMeta.classList.add("post-meta");
+    myPostHeader.append(myPostMeta);
+    myPostMetaIcon.classList.add("post-meta__icon");
+    myPostMeta.append(myPostMetaIcon);
+});
+
+
