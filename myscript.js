3b2f5fce826536afbb73d075aef7c0f7321ab47c
vince <vincedinardo21@gmail.com>
2022-05-20 17:19:08 +0200

    Nomi autori

--- myscript.js
+++ myscript.js
@@ -5,12 +5,22 @@ posts.forEach((element) => {
     let myPost = document.createElement("div");
     let myPostHeader = document.createElement("div");
     let myPostMeta = document.createElement("div");
+
     let myPostMetaIcon = document.createElement("div");
 
-    let myImg = document.createElement("img");
-    myImg.src = element.author.image;
-    
+    // Meta Data Div
+    let myPostMetaData = document.createElement("div");
+    // Nome autore e time
+    let authorName = document.createElement("div");
+    authorName.append(element.author.name);
+    //let myPostMetaAuthor = document.createElement("div");
+    let myPostMetaTime = document.createElement("div");
 
+    // Creo l'immagine
+    let myImg = document.createElement("img");
+    myImg.src = element.media;
+   
+    // Creo la concatenazione di div
     myPost.classList.add("post");
     myContainer.append(myPost);
     myPostHeader.classList.add("post__header");
@@ -18,7 +28,16 @@ posts.forEach((element) => {
     myPostMeta.classList.add("post-meta");
     myPostHeader.append(myPostMeta);
     myPostMetaIcon.classList.add("post-meta__icon");
+ 
     myPostMeta.append(myPostMetaIcon);
+    myPostMetaData.classList.add("post-meta__data");
+    myPostMeta.append(myPostMetaData);
+    authorName.classList.add("post-meta__author");
+    myPostMetaData.append(authorName);
+    myPostMetaData.append(myPostMetaTime);
+    
+
+    myImg.classList.add("profile-pic");
     myPostMetaIcon.append(myImg);
 });
 
